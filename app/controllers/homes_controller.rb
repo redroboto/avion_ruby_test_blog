8f9aa363edc938f77f28c59dd23de1fc9f0a8368
redroboto <vince.pleyto@gmail.com>
2024-05-18 00:37:38 +0800

    feat: add CRUD for /posts

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,9 +3,11 @@ class HomesController < ApplicationController
   # GET /homes or /homes.json
   def index
     @greeting = 'Hello'
+    @current_user = 'guest'
   end
 
   def about
+    @current_user = 'guest in about page'
   end
 
 end  
